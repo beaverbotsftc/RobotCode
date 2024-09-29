b14b77e7ea8b55228024438e1a308e633b5bd1e3
DevNoobus <82532732+DevNoobus@users.noreply.github.com>
2024-09-29 18:26:30 -0400

    Update MotorController.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/MotorController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/MotorController.java
@@ -1,5 +1,27 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 public class MotorController {
-    //controls motor, so that it makes life easier.
+    public DcMotor drivenMotor;
+    public float lsPosition = 0;
+
+    public class DriveDistance {
+
+    }
+
+    public class DrivePosition {
+
+    }
+
+    public float lsGetPosition(aramType param) {
+
+        return lsPosition; // Of type RetType
+    }
+
+    public class LSSetPosition {
+    public float Distance;
+    // Build system where motor moves to a position between 0 and 1 or inches
+    }
 }
