abc3dbd0086e3aacd6f89647ae20f3d148eb26d6
acolguyu <160174394+acolguyu@users.noreply.github.com>
2025-01-21 17:01:36 -0500

    Deleted old pathfollower + created 2 new classes in the collections folder that will be used for the main teleOP.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/BasicTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/BasicTeleOp.java
@@ -26,6 +26,7 @@ public class BasicTeleOp extends LinearOpMode {
         // Initialize the hardware variables. Note that the strings used here must correspond
         // to the names assigned during the robot configuration step on the DS or RC devices.
         motors.init(hardwareMap);
+        subsystems.init(hardwareMap);
 
         // Wait for the game to start (driver presses START)
         telemetry.addData("Status", "Initialized");
