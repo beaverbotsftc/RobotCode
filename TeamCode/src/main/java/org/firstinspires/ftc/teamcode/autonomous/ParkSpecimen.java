d327b1c820a4df7b4ef7036933efd9455e1ffb33
Graisen Edwards <graisen.edwards@gmail.com>
2025-01-29 21:17:19 -0500

    Specimen side parking only auto, tuning tbd

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/ParkSpecimen.java
@@ -0,0 +1,58 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+import org.firstinspires.ftc.teamcode.collections.Motors;
+import org.firstinspires.ftc.teamcode.collections.Sensors;
+import org.firstinspires.ftc.teamcode.pathfollower2.DOFs;
+import org.firstinspires.ftc.teamcode.pathfollower2.MathUtils;
+import org.firstinspires.ftc.teamcode.pathfollower2.PID;
+import org.firstinspires.ftc.teamcode.pathfollower2.Path;
+import org.firstinspires.ftc.teamcode.pathfollower2.PathFollower;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+
+@Autonomous(name = "Park (Specimen side)")
+public class ParkSpecimen extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        Motors motors = new Motors();
+        Sensors sensors = new Sensors();
+        motors.init(hardwareMap);
+        sensors.init(hardwareMap);
+
+        sensors.odometry.setPosition(new Pose2D(DistanceUnit.INCH, 0, 72, AngleUnit.DEGREES, 0));
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.speak("I have been initialized!");
+        telemetry.update();
+        waitForStart();
+
+        Path path = new Path.PathBuilder(this::isStopRequested)
+                .linearTo(new HashMap<DOFs.DOF, Double>() {{
+                    put(DOFs.DOF.X, 0.0);
+                    put(DOFs.DOF.Y, 120.0);
+                    put(DOFs.DOF.THETA, 0.0);
+                }}, 8)
+                .build();
+
+        PathFollower pathFollower = new PathFollower(path, new DOFs(sensors.odometry, motors), new HashMap<DOFs.DOF, PID.K>() {{
+            put(DOFs.DOF.X, new PID.K(1, 0.1, 0.05));
+            put(DOFs.DOF.Y, new PID.K(1, 0.1, 0.05));
+            put(DOFs.DOF.THETA, new PID.K(1, 0.1, 0.05));
+        }}, new HashMap<DOFs.DOF, PathFollower.K>() {{
+            put(DOFs.DOF.X, new PathFollower.K(TuningConstants.x, 1));
+            put(DOFs.DOF.Y, new PathFollower.K(TuningConstants.y, 1));
+            put(DOFs.DOF.THETA, new PathFollower.K(TuningConstants.theta, 1));
+        }});
+
+        pathFollower.run(telemetry);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/TuningConstants.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+public class TuningConstants {
+    public static final double x = 1;
+    public static final double y = 1;
+    public static final double theta = 1;
+}
