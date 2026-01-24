16eddb5c40ebcbbfd06ae8adcb99c84d63947431
Graisen Edwards <graisen.edwards@gmail.com>
2026-01-24 14:40:25 -0500

    IntakeControl made simpler, less features, to be added later

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/IntakeControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/IntakeControl.java
@@ -42,58 +42,22 @@ public class IntakeControl implements Command {
     public boolean periodic() {
         double intakeSpeed = gamepad.getRightTrigger() - gamepad.getLeftTrigger();
 
-        Pair<List<Double>, List<Double>> robot = !restrict ? null : Geometry.generateBox(localizer.getPosition().getX(), localizer.getPosition().getY(), Constants.ROBOT_WIDTH, Constants.ROBOT_LENGTH, localizer.getPosition().getTheta());
-
-        DrivetrainState goalPosition = new DrivetrainState(Constants.GOAL_X, side == Side.RED ? Constants.GOAL_Y : -Constants.GOAL_Y, 0);
-        DrivetrainState position = !restrict ? null : localizer.getPosition();
-        double distanceToGoal = !restrict ? Double.NaN : goalPosition.lateralDistance(position);
-        double allowedError = !restrict ? Double.NaN : distanceToGoal > 117.0 ? 1.5 : 5.0;
-
-        double angularError = !restrict ? Double.NaN : Math.abs(Localizer.wind(position.angleTo(goalPosition), localizer.getPosition().getTheta()) - Constants.shooterBias - localizer.getPosition().getTheta());
-
-        if (angularError < Math.toRadians(300) / distanceToGoal)
-            led.setBallLedPurple();
-        else
-            led.turnOffBallLed();
-
-
-        if (gamepad.getRightBumper()
-                && (!restrict || true ||
-                Geometry.polygonPolygonIntersects(Constants.SHOOTING_ZONE_NEAR_X, Constants.SHOOTING_ZONE_NEAR_Y, robot.first, robot.second)
-                || Geometry.polygonPolygonIntersects(Constants.SHOOTING_ZONE_FAR_X, Constants.SHOOTING_ZONE_FAR_Y, robot.first, robot.second))
-        ) { // If in the shooting zone
-            if (!restrict || angularError <
-                    Math.toRadians(150) / distanceToGoal
-            ) {
-                //intake.setMaxPower(0.95);
-                intake.setMaxPower(1);
-                stopper.setMaxPower(0.95);
-                if (!restrict || (ShooterControl.percentError < allowedError && ShooterControl.percentError >= 0.0)) {
-                    intakeSpeed = 1.0;
-                    stopper.spinForward();
-                    intake.empty();
-                } else {
-                    intakeSpeed = 0.0;
-                    stopper.stop();
-                }
-            }
-        } else if (intakeSpeed != 0) { //when intaking balls
-            intake.setMaxPower(1);
-            stopper.setMaxPower(0.5);
-
-            stopper.spinReverse();
-        } else {
-            stopper.stop();
-            intake.spin(0);
-        }
-
-        if (intakeSpeed > 0) {
-            if (!intake.full())
-                intake.spin(intakeSpeed);
-            else
-                intake.spin(0);
-        } else
+        if (gamepad.getRightBumper()) {
+            intake.spin(1.0);
+            stopper.spinForward();
             intake.empty();
+        } else {
+            intake.spin(intakeSpeed);
+            if (intakeSpeed == 0) {
+                stopper.spin(0);
+            } else {
+                stopper.spin(-0.8);
+            }
+
+            if (intakeSpeed < 0) {
+                intake.empty();
+            }
+        }
 
         return false;
     }
